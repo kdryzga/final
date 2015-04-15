f74e27f282be409489dc4b4e061e79312ca7179f
kdryzga <kevindryzga@ip0af50071.int.uvm.edu>
2015-04-15 19:40:16 -0400

    first commit eva

---
+++ MyFirstClass.java
@@ -0,0 +1,54 @@
+/* Jackie Horton
+ CS 110
+ My first JAVA program
+*/
+import java.util.*;
+// or import java.util.Scanner;
+
+public class MyFirstClass 
+{
+   
+   public static void main(String [] args)
+   {
+      //Code Goes here
+      //1-13-15
+      String message ="Have a great day!";
+      System.out.println(message.charAt(5));
+      
+                         
+      
+      /*int x;
+      x = 6;
+      float f;
+      f = 6.6f;
+      double d;
+      d = 6.6;
+      char ch, ch1, ch2;
+      ch = 'a';
+      //System.out.println(ch); //println includes a new line
+      
+      
+      //1-15-15
+      Scanner s;
+      s = new Scanner(System.in);//creates the object with the address 's'
+      x = s.nextInt();
+      int y = s.nextInt();
+      
+      //System.out.println(x+y);
+      
+      int a = 5;
+      int b = 2;
+      
+      System.out.println(a/b); //integers are truncated when divided
+      System.out.println((double)a/b); //cast to change types
+      System.out.println(a%b); //remainder
+      
+      double d1 = 5.0;
+      double d2 = 2.0;
+      
+      System.out.println(d1/d2);
+      
+      */
+      
+   }
+}
